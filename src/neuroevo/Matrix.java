14631ce75f004800ae29ac36323dd6a25534ca14
Temila <temila.ms06f@gmail.com>
2016-05-11 22:43:31 +0200

    Matrix bug fixed

--- src/neuroevo/Matrix.java
+++ src/neuroevo/Matrix.java
@@ -13,7 +13,7 @@ public class Matrix {
 	public Matrix(ArrayList<Double> list, int row, int col){
 		matrix = new Double[row][col];
 		for(int i = 0;i < row;i ++){
-			ArrayList<Double> sub_list = (ArrayList<Double>) list.subList(i*col, (i + 1)*col);
+			ArrayList<Double> sub_list = new ArrayList<Double>(list.subList(i*col, (i + 1)*col));
 			matrix[i] = sub_list.toArray(new Double[col]);
 		}
 	}
@@ -31,12 +31,6 @@ public class Matrix {
 	 */
 	public Matrix(Double[][] array){
 		this.matrix = array;
-	}
-	
-	public Matrix Times(Matrix M){
-		Double[][] input_matrix = M.getMatrix();
-		
-		return null;
 	}
 	
 	public void Plus(Matrix M) {
@@ -69,21 +63,24 @@ public class Matrix {
 		}
 	}
 	
-	public void mulMatrix(Matrix M) {
+	public void Times(Matrix M) {
 		Double[][] input_matrix = M.getMatrix();
 		int n = input_matrix.length;
 		int m = input_matrix[0].length;
+		Double[][] output_matrix = new Double[matrix.length][m];
 		if (n != matrix[0].length) {
 			System.out.println("fail to multiply, dimension mismatch");
 		} else {
 			for (int i = 0; i < matrix.length; i++) {
 				for (int j = 0; j < m; j++) {
+					output_matrix[i][j] = 0.0;
 					for (int k = 0; k < n; k++) {
-						matrix[i][j] += (matrix[i][k] * input_matrix[k][j]);
+						System.out.println(output_matrix[i][j]);
 					}
 				}
 			}
 		}
+		matrix = output_matrix;
 	}
 	
 	public Double[][] getMatrix(){
